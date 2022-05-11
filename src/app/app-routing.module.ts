7785495c835139914bdd34bd2da074c8b6988b9e
aklaani <aeham-k@hotmail.com>
2022-05-11 13:40:49 +0300

    navbar upgraded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,12 @@ import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import {LoginPageComponent} from "./login-page/login-page.component";
 import {RegisterComponent} from "./register/register.component";
 import {AppComponent} from "./app.component";
-import {HomePageComponent} from "./home-page/home-page.component";
+import {MenuComponent} from "./menu/menu.component";
 
 const routes: Routes = [
   {path : 'login', component:LoginPageComponent},
-  {path : 'test', component:HomePageComponent},
-  {path : 'register', component:RegisterComponent}
+  {path : 'register', component:RegisterComponent},
+  {path : 'menu', component:MenuComponent}
 ]; // sets up routes constant where you define your routes
 
 @NgModule({
